f5406b50ce00366121261e6ee5cce9553ba05ac9
Hlamong55 <bijoymarma55@gmail.com>
2026-06-14 23:54:47 +0600

    Solve Two Sum using brute force approach

---
+++ Beginner/two_sum.py
@@ -0,0 +1,28 @@
+"""
+Problem: Two Sum
+
+Given an array of integers nums and an integer target,
+return indices of the two numbers such that they add up to target.
+
+Example:
+nums = [2, 7, 11, 15]
+target = 9
+
+Output:
+[0, 1]
+
+Explanation:
+nums[0] + nums[1] = 2 + 7 = 9
+"""
+
+def two_sum(nums, target):
+    # TODO: Implement solution
+    pass
+
+
+# Test Case
+nums = [2, 7, 11, 15]
+target = 9
+
+result = two_sum(nums, target)
+print(result)
