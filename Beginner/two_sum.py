0852fb8a0a0b3c421fb53f589033daec97c91340
Hlamong55 <bijoymarma55@gmail.com>
2026-06-16 22:56:30 +0600

    changes updated in two_sum problem

--- Beginner/two_sum.py
+++ Beginner/two_sum.py
@@ -3,21 +3,13 @@ Problem: Two Sum
 
 Given an array of integers nums and an integer target,
 return indices of the two numbers such that they add up to target.
-
-Example:
-nums = [2, 7, 11, 15]
-target = 9
-
-Output:
-[0, 1]
-
-Explanation:
-nums[0] + nums[1] = 2 + 7 = 9
 """
 
 def two_sum(nums, target):
-    # TODO: Implement solution
-    pass
+    for i in range(len(nums)):
+        for j in range(i + 1, len(nums)):
+            if nums[i] + nums[j] == target:
+                return [i, j]
 
 
 # Test Case
